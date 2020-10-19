f6b421da605a634bcd19363d42ef2e39d0144357
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-10-19 18:10:03 +0900

    Update Runtime

--- src/runtime/Runtime.ts
+++ src/runtime/Runtime.ts
@@ -1,13 +1,41 @@
 import { Entity } from '@/models/entity/Entity';
+import { SceneViewModel } from '@/viewModels/SceneViewModel';
+import { Disposable } from 'externals/EditingSystemTs/src/TypedEvent';
+import { Camera } from 'three/src/cameras/Camera';
+import { dic } from '@/di/dic';
+import { ViewportRenderer } from './ViewportRenderer';
 
-export class Runtime {
-  private target: Entity | null = null;
+export class Runtime implements Disposable {
+  private rootSceneViewModel: SceneViewModel;
+  private camera: Camera | null = null;
 
-  setup(target: Entity): void {
-    this.target = target;
+  private renderer: ViewportRenderer;
+  private isDisposed = false;
+
+  constructor(rootScene: Entity, canvas: HTMLCanvasElement) {
+    this.rootSceneViewModel = dic().resolve(SceneViewModel);
+    this.rootSceneViewModel.setup(rootScene);
+
+    this.renderer = new ViewportRenderer(canvas);
+
+    requestAnimationFrame(() => this.animate());
   }
 
-  start(): void {
-    //
+  dispose(): void {
+    this.isDisposed = true;
+
+    this.rootSceneViewModel.dispose();
+  }
+
+  private animate(): void {
+    if (this.isDisposed) {
+      return;
+    }
+
+    if (this.camera == null) {
+      return;
+    }
+
+    this.renderer.requestRender(this.rootSceneViewModel, this.camera);
   }
 }
