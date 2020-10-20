fe5c5f65a85c18151e8f660bd4fb8f51f8635ec4
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-10-20 15:17:13 +0900

    Move registerEntityDefinitions

--- src/interactors/project/CreateEntityInteractor.ts
+++ src/interactors/project/CreateEntityInteractor.ts
@@ -2,10 +2,16 @@ import { CreateEntityUseCase } from '@/useCases/project/CreateEntityUseCase';
 import { EntityCreator } from '@/models/entity/EntityCreator';
 import { singleton } from 'tsyringe';
 import { Entity } from '@/models/entity/Entity';
+import { ChildEntityTag, EntityDefinition } from '@/models/entity/EntityDefinition';
+import { MeshTypes, RenderDefinition } from '@/models/entity/RenderDefinition';
+import { Vector3 } from '@/foundations/math/Vector3';
+import { ScriptDefinition } from '@/models/entity/ScriptDefinition';
 
 @singleton()
 export class CreateEntityInteractor implements CreateEntityUseCase {
-  constructor(private readonly entityCreator: EntityCreator) {}
+  constructor(private readonly entityCreator: EntityCreator) {
+    this.registerEntityDefinitions();
+  }
 
   invoke(name: string): Entity {
     const entity = this.entityCreator.create(name);
@@ -16,4 +22,59 @@ export class CreateEntityInteractor implements CreateEntityUseCase {
   }
 
   private static instanceCount = 0;
+
+  private registerEntityDefinitions(): void {
+    const box = new EntityDefinition({
+      name: 'box',
+      renderDefinition: new RenderDefinition({ meshType: MeshTypes.Box }),
+      scriptDefinition: new ScriptDefinition({ code: CreateEntityInteractor.testScriptCode }),
+    });
+
+    const point = new EntityDefinition({
+      name: 'point',
+      renderDefinition: new RenderDefinition({ meshType: MeshTypes.Point }),
+    });
+
+    const havingChildrenBox = new EntityDefinition({
+      name: 'havingChildrenBox',
+      renderDefinition: new RenderDefinition({ meshType: MeshTypes.Point }),
+
+      childTags: [
+        new ChildEntityTag({ definitionName: 'box', position: new Vector3(5, 0, 0) }),
+        new ChildEntityTag({ definitionName: 'point', position: new Vector3(0, 0, 5) }),
+        new ChildEntityTag({ definitionName: 'box', position: new Vector3(-5, 0, 0) }),
+        new ChildEntityTag({ definitionName: 'point', position: new Vector3(0, 0, -5) }),
+      ],
+    });
+
+    const threeGens = new EntityDefinition({
+      name: 'threeGens',
+      renderDefinition: new RenderDefinition({ meshType: MeshTypes.Point }),
+
+      childTags: [
+        new ChildEntityTag({
+          definitionName: 'box',
+          position: new Vector3(5, 0, 0),
+          childTags: [
+            new ChildEntityTag({
+              definitionName: 'point',
+              position: new Vector3(0, 0, 10),
+              childTags: [new ChildEntityTag({ definitionName: 'box', position: new Vector3(5, 0, 0) })],
+            }),
+          ],
+        }),
+      ],
+    });
+
+    this.entityCreator.addDefinition(box);
+    this.entityCreator.addDefinition(point);
+    this.entityCreator.addDefinition(havingChildrenBox);
+    this.entityCreator.addDefinition(threeGens);
+  }
+
+  private static testScriptCode = `
+    function update(entity, event) {
+      entity.rotation = new Vector3(entity.rotation.x + 10, entity.rotation.y, entity.rotation.z);
+    }
+  `;
 }
--- src/models/entity/EntityCreator.ts
+++ src/models/entity/EntityCreator.ts
@@ -1,69 +1,19 @@
 import { MathHelper } from '@/foundations/math/MathHelper';
-import { Vector3 } from '@/foundations/math/Vector3';
 import { singleton } from 'tsyringe';
 import { Entity } from './Entity';
 import { ChildEntityTag, EntityDefinition } from './EntityDefinition';
-import { MeshTypes, RenderDefinition } from './RenderDefinition';
 import { dic } from '@/di/dic';
 
 @singleton()
 export class EntityCreator {
-  constructor() {
-    this.registerEntityDefinitions();
-  }
+  private readonly entityDefinitions = new Map<string, Readonly<EntityDefinition>>();
 
   create(definitionName: string): Entity {
     return this.createInternal(definitionName, true);
   }
 
-  private readonly entityDefinitions = new Map<string, EntityDefinition>();
-
-  private registerEntityDefinitions(): void {
-    const box = new EntityDefinition({
-      name: 'box',
-      renderDefinition: new RenderDefinition({ meshType: MeshTypes.Box }),
-    });
-
-    const point = new EntityDefinition({
-      name: 'point',
-      renderDefinition: new RenderDefinition({ meshType: MeshTypes.Point }),
-    });
-
-    const havingChildrenBox = new EntityDefinition({
-      name: 'havingChildrenBox',
-      renderDefinition: new RenderDefinition({ meshType: MeshTypes.Point }),
-
-      childTags: [
-        new ChildEntityTag({ definitionName: 'box', position: new Vector3(5, 0, 0) }),
-        new ChildEntityTag({ definitionName: 'point', position: new Vector3(0, 0, 5) }),
-        new ChildEntityTag({ definitionName: 'box', position: new Vector3(-5, 0, 0) }),
-        new ChildEntityTag({ definitionName: 'point', position: new Vector3(0, 0, -5) }),
-      ],
-    });
-
-    const threeGens = new EntityDefinition({
-      name: 'threeGens',
-      renderDefinition: new RenderDefinition({ meshType: MeshTypes.Point }),
-
-      childTags: [
-        new ChildEntityTag({
-          definitionName: 'box',
-          position: new Vector3(5, 0, 0),
-          childTags: [
-            new ChildEntityTag({
-              definitionName: 'point',
-              position: new Vector3(0, 0, 10),
-              childTags: [new ChildEntityTag({ definitionName: 'box', position: new Vector3(5, 0, 0) })],
-            }),
-          ],
-        }),
-      ],
-    });
-
-    this.entityDefinitions.set(box.name, box);
-    this.entityDefinitions.set(point.name, point);
-    this.entityDefinitions.set(havingChildrenBox.name, havingChildrenBox);
-    this.entityDefinitions.set(threeGens.name, threeGens);
+  addDefinition(definition: Readonly<EntityDefinition>): void {
+    this.entityDefinitions.set(definition.name, definition);
   }
 
   private createInternal(definitionName: string, isOwner: boolean): Entity {
