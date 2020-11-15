39b6836ace2c3b4a4aea198f4b260ce77852ff7b
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-11-16 08:34:20 +0900

    Vue.config.devtools = process.env.NODE_ENV === 'development';

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata';
 import Vue from 'vue';
 import App from '@/App.vue';
 
+Vue.config.devtools = process.env.NODE_ENV === 'development';
+
 //  composition-api
 import VueCompositionApi from '@vue/composition-api';
 Vue.use(VueCompositionApi);
